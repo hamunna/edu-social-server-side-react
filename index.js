2d0b4f45cdac3968ce65dde75e536679841217a5
hamunna <hamunna@gmail.com>
2021-12-19 11:48:26 +0600

    MongoDB Connected

--- index.js
+++ index.js
@@ -9,13 +9,29 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xribv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-
+async function run() {
+	try {
+		await client.connect();
+		const database = client.db('sample_mflix');
+		const movies = database.collection('movies');
+		// Query for a movie that has the title 'Back to the Future'
+		const query = { title: 'Back to the Future' };
+		const movie = await movies.findOne(query);
+		console.log(movie);
+	} finally {
+		// Ensures that the client will close when you finish/error
+		await client.close();
+	}
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
 	res.send('EduSocial Server is running...');
 });
 
 app.listen(port, () => {
-	console.log('Server is running on: ', port)
+	console.log('Server is running on port: ', port)
 });
